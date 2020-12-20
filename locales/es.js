e8cc6e29c0c77cdd98817fc6f1c8c487047d382e
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-12-20 22:56:45 +0100

    #3 Definición de literales para los juegos de la página de inicio

--- locales/es.js
+++ locales/es.js
@@ -39,6 +39,24 @@ export default {
   VIEWS: {
     HOME: {
       TITLE: 'Inicio',
+      WELCOME: {
+        TEXT: '<p>En esta aplicación web encontrarás multitud de estadísticas asociadas a diferentes loterías de España. Actualmente damos soporte a La Quiniela, Bonoloto, Primitiva, El Gordo de la Primitiva y Euromillones. Podrás consultar todo tipo de estadísticas que te facilitarán la tarea de hacer tus apuestas y con mayores garantías de éxito.</p>',
+        QUINIELA: {
+          TEXT: 'Quiniela',
+        },
+        BONOLOTO: {
+          TEXT: 'Bonoloto',
+        },
+        PRIMITIVA: {
+          TEXT: 'Primitiva',
+        },
+        GORDO: {
+          TEXT: 'El Gordo',
+        },
+        EUROMILLONES: {
+          TEXT: 'Euromillones',
+        },
+      },
     },
   },
 };
