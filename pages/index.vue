6243023539aff1cecee66fdc9168583d36e4c800
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-12-22 00:39:16 +0100

    #3 Implementación de sección con webs recomendadas en la página de inicio

--- pages/index.vue
+++ pages/index.vue
@@ -67,6 +67,40 @@
             />
           </v-card-text>
         </v-card>
+        <v-card class="mt-3" flat>
+          <v-card-title class="blue--text">
+            {{ recommendedWebsTitle }}
+          </v-card-title>
+          <v-card-text class="text-justify">
+            {{ recommendedWebsText }}
+          </v-card-text>
+        </v-card>
+        <v-responsive id="recommended-webs">
+          <v-row
+            dense
+            justify="center"
+          >
+            <v-col
+              v-for="(web, i) in recommendedWebs"
+              :key="web.name"
+              class="recommended-web d-flex align-center"
+              cols="auto"
+              md="4"
+            >
+              <a
+                :href="web.url"
+                target="_blank"
+                :title="web.name"
+                rel="nofollow"
+              >
+                <img
+                  :src="recommendedWebs[i].img"
+                  :alt="web.alt"
+                >
+              </a>
+            </v-col>
+          </v-row>
+        </v-responsive>
       </div>
     </v-col>
   </v-row>
@@ -91,6 +125,34 @@ export default {
       primitivaText: this.$t('VIEWS.HOME.GAMES.PRIMITIVA.TEXT'),
       gordoText: this.$t('VIEWS.HOME.GAMES.GORDO.TEXT'),
       euromillonesText: this.$t('VIEWS.HOME.GAMES.EUROMILLONES.TEXT'),
+      recommendedWebsTitle: this.$t('VIEWS.HOME.RECOMMENDED_WEBS.TITLE'),
+      recommendedWebsText: this.$t('VIEWS.HOME.RECOMMENDED_WEBS.TEXT'),
+      recommendedWebs: [
+        {
+          name: '¿Cuándo me va a tocar?',
+          img: '/images/recommended_web-cuando_me_va_a_tocar.png',
+          url: 'https://www.cuandomevaatocar.com',
+          alt: 'Logo de "¿Cuándo me va a tocar?"',
+        },
+        {
+          name: 'Estadística para todos',
+          img: '/images/recommended_web-estadistica_para_todos.png',
+          url: 'http://www.estadisticaparatodos.es/taller/loterias/loterias.html',
+          alt: 'Logo de "Estadística para todos"',
+        },
+        {
+          name: 'Combinación ganadora',
+          img: '/images/recommended_web-combinacion_ganadora.png',
+          url: 'https://www.combinacionganadora.com/',
+          alt: 'Logo de "Combinación ganadora"',
+        },
+        {
+          name: 'LOTERIAS.COM',
+          img: '/images/recommended_web-loterias.com.ico',
+          url: 'https://www.loterias.com',
+          alt: 'Logo de "LOTERIAS.COM"',
+        },
+      ],
     };
   },
   head() {
@@ -105,6 +167,20 @@ export default {
 #games{
   .v-btn{
     margin: 3px 0 3px;
+  }
+}
+
+#recommended-webs{
+  .recommended-web{
+    a{
+      display: block;
+      width: 100%;
+
+      img{
+        max-width: 100%;
+        margin: 0 auto;
+      }
+    }
   }
 }
 </style>
