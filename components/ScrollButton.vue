3fae29c646809780a1d9be707a091a7c01b00e41
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2021-01-03 22:00:53 +0100

    #4 Implementación de componente para mostrar botón que realice scroll hasta la cima de la página

---
+++ components/ScrollButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-fab-transition>
+    <v-btn
+      v-show="fab"
+      v-scroll="onScroll"
+      fab
+      dark
+      fixed
+      bottom
+      right
+      color="success"
+      @click="toTop"
+    >
+      <v-icon>mdi-chevron-up</v-icon>
+    </v-btn>
+  </v-fab-transition>
+</template>
+
+<script>
+export default {
+  name: 'ScrollButton',
+  data() {
+    return {
+      fab: false,
+    };
+  },
+  methods: {
+    onScroll(e) {
+      if (typeof window === 'undefined') return;
+      const top = window.pageYOffset || e.target.scrollTop || 0;
+      this.fab = top > 20;
+    },
+    toTop() {
+      this.$vuetify.goTo(0);
+    },
+  },
+};
+</script>
