4b6ef12e0ce50dcf8e96c0024dac09061d603887
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2021-03-31 20:43:24 +0200

    #15 (components/BonolotoLastDateByReimbursementStatsTable): Implementación de componente para visualizar las estadísticas de última aparición por reintegro

---
+++ components/bonoloto/stats/BonolotoLastDateByReimbursementStatsTable.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="lastDateByReimbursementStats.data"
+      :options.sync="options"
+      :server-items-length="lastDateByReimbursementStats.total"
+      :loading="loading"
+      class="elevation-1"
+    >
+      <template
+        v-slot:[`item.reimbursement`]="{ item }"
+      >
+        <v-chip
+          color="primary"
+        >
+          {{ item.reintegro }}
+        </v-chip>
+      </template>
+      <template
+        v-slot:[`item.date`]="{ item }"
+      >
+        {{ getFormattedDate(item.fecha) }}
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+
+import getFormattedDate from '@/mixins/getFormattedDate';
+
+export default {
+  name: 'BonolotoLastDateByReimbursementStatsTable',
+  mixins: [
+    getFormattedDate,
+  ],
+  data() {
+    return {
+      options: {
+        mustSort: true,
+        sortBy: ['date'],
+        sortDesc: [false],
+        itemsPerPage: this.$store.state.bonoloto.statsPagination.per_page,
+      },
+      headers: [
+        {
+          text: this.$t('VIEWS.BONOLOTO.STATS.LAST_DATE_BY_REIMBURSEMENT.TABLE.REIMBURSEMENT.LABEL'),
+          align: 'center',
+          sortable: true,
+          value: 'reimbursement',
+        },
+        {
+          text: this.$t('VIEWS.BONOLOTO.STATS.LAST_DATE_BY_REIMBURSEMENT.TABLE.DATE.LABEL'),
+          align: 'center',
+          sortable: true,
+          value: 'date',
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState('bonoloto', {
+      loading: 'loading',
+      pagination: 'statsPagination',
+    }),
+    ...mapGetters('bonoloto', {
+      lastDateByReimbursementStats: 'getLastDateByReimbursementStats',
+    }),
+  },
+  watch: {
+    options: {
+      handler() {
+        this.getLastDateByReimbursementStats();
+      },
+      deep: true,
+    },
+  },
+  destroyed() {
+    this.$store.dispatch('bonoloto/destroyStats');
+  },
+  methods: {
+    getLastDateByReimbursementStats() {
+      const {
+        sortBy, sortDesc, page, itemsPerPage,
+      } = this.options;
+
+      this.$store.commit('bonoloto/setStatsPagination', {
+        page,
+        per_page: itemsPerPage,
+        sort_type: sortDesc[0] ? 'desc' : 'asc',
+        sort_property: sortBy[0],
+      });
+
+      this.$store.dispatch('bonoloto/getLastDateByReimbursementStats');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
