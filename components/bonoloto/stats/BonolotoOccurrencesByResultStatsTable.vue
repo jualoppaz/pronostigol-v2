fa2121e426a2f203f343bc133d8697da1a00bd65
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2021-03-31 17:18:50 +0200

    #15 (components/Bonoloto/OccurrencesByResultStatsTable): Implementación de componente para visualizar las estadísticas de apariciones por resultado

---
+++ components/bonoloto/stats/BonolotoOccurrencesByResultStatsTable.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="occurrencesByResultStats.data"
+      :options.sync="options"
+      :server-items-length="occurrencesByResultStats.total"
+      :loading="loading"
+      class="elevation-1"
+    >
+      <template
+        v-slot:[`item.result`]="{ item }"
+      >
+        <v-chip
+          v-for="number in item.resultado"
+          :key="number.numero"
+          color="primary"
+        >
+          {{ getFormattedNumber(number.numero) }}
+        </v-chip>
+      </template>
+      <template
+        v-slot:[`item.occurrences`]="{ item }"
+      >
+        {{ item.apariciones }}
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+import getFormattedNumber from '@/mixins/getFormattedNumber';
+
+export default {
+  name: 'BonolotoStatsTable',
+  mixins: [
+    getFormattedNumber,
+  ],
+  data() {
+    return {
+      options: {
+        mustSort: true,
+        sortBy: ['occurrences'],
+        sortDesc: [true],
+        itemsPerPage: this.$store.state.bonoloto.statsPagination.per_page,
+      },
+      headers: [
+        {
+          text: this.$t('VIEWS.BONOLOTO.STATS.OCCURRENCES_BY_RESULT_STATS.TABLE.RESULT.LABEL'),
+          align: 'center',
+          sortable: true,
+          value: 'result',
+        },
+        {
+          text: this.$t('VIEWS.BONOLOTO.STATS.OCCURRENCES_BY_RESULT_STATS.TABLE.OCCURRENCES.LABEL'),
+          align: 'center',
+          sortable: true,
+          value: 'occurrences',
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState('bonoloto', {
+      loading: 'loading',
+      pagination: 'statsPagination',
+      occurrencesByResultStats: (state) => state.stats.occurrencesByResult,
+    }),
+  },
+  watch: {
+    options: {
+      handler() {
+        this.getOccurrencesByResultStats();
+      },
+      deep: true,
+    },
+  },
+  destroyed() {
+    this.$store.dispatch('bonoloto/destroyStats');
+  },
+  methods: {
+    getOccurrencesByResultStats() {
+      const {
+        sortBy, sortDesc, page, itemsPerPage,
+      } = this.options;
+
+      this.$store.commit('bonoloto/setStatsPagination', {
+        page,
+        per_page: itemsPerPage,
+        sort_type: sortDesc[0] ? 'desc' : 'asc',
+        sort_property: sortBy[0],
+      });
+
+      this.$store.dispatch('bonoloto/getOccurrencesByResultStats');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
