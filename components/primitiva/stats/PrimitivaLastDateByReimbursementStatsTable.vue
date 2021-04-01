fcf25c4d50d4ad6fddb1c262ebdeda18183eb5c7
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2021-04-01 17:09:18 +0200

    #18 (components/primitiva/*): Implementación de los distintos componentes para la consulta de estadísticas de la Primitiva

---
+++ components/primitiva/stats/PrimitivaLastDateByReimbursementStatsTable.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="lastDateByReimbursementStats.data"
+      :options.sync="options"
+      :server-items-length="lastDateByReimbursementStats.total"
+      :loading="loading"
+      class="elevation-1"
+    >
+      <template v-slot:progress>
+        <v-progress-linear
+          indeterminate
+          absolute
+          color="green darken-2"
+        />
+      </template>
+      <template
+        v-slot:[`item.reimbursement`]="{ item }"
+      >
+        <v-chip
+          color="primary"
+        >
+          {{ item.reintegro }}
+        </v-chip>
+      </template>
+      <template
+        v-slot:[`item.date`]="{ item }"
+      >
+        {{ getFormattedDate(item.fecha) }}
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+
+import getFormattedDate from '@/mixins/getFormattedDate';
+
+export default {
+  name: 'PrimitivaLastDateByReimbursementStatsTable',
+  mixins: [
+    getFormattedDate,
+  ],
+  data() {
+    return {
+      options: {
+        mustSort: true,
+        sortBy: ['date'],
+        sortDesc: [false],
+        itemsPerPage: this.$store.state.primitiva.statsPagination.per_page,
+      },
+      headers: [
+        {
+          text: this.$t('VIEWS.PRIMITIVA.STATS.LAST_DATE_BY_REIMBURSEMENT.TABLE.REIMBURSEMENT.LABEL'),
+          align: 'center',
+          sortable: true,
+          value: 'reimbursement',
+        },
+        {
+          text: this.$t('VIEWS.PRIMITIVA.STATS.LAST_DATE_BY_REIMBURSEMENT.TABLE.DATE.LABEL'),
+          align: 'center',
+          sortable: true,
+          value: 'date',
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState('primitiva', {
+      loading: 'loading',
+      pagination: 'statsPagination',
+    }),
+    ...mapGetters('primitiva', {
+      lastDateByReimbursementStats: 'getLastDateByReimbursementStats',
+    }),
+  },
+  watch: {
+    options: {
+      handler() {
+        this.getLastDateByReimbursementStats();
+      },
+      deep: true,
+    },
+  },
+  destroyed() {
+    this.$store.dispatch('primitiva/destroyStats');
+  },
+  methods: {
+    getLastDateByReimbursementStats() {
+      const {
+        sortBy, sortDesc, page, itemsPerPage,
+      } = this.options;
+
+      this.$store.commit('primitiva/setStatsPagination', {
+        page,
+        per_page: itemsPerPage,
+        sort_type: sortDesc[0] ? 'desc' : 'asc',
+        sort_property: sortBy[0],
+      });
+
+      this.$store.dispatch('primitiva/getLastDateByReimbursementStats');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ components/primitiva/stats/PrimitivaOccurrencesByResultStatsTable.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="occurrencesByResultStats.data"
+      :options.sync="options"
+      :server-items-length="occurrencesByResultStats.total"
+      :loading="loading"
+      class="elevation-1"
+    >
+      <template v-slot:progress>
+        <v-progress-linear
+          indeterminate
+          absolute
+          color="green darken-2"
+        />
+      </template>
+      <template
+        v-slot:[`item.result`]="{ item }"
+      >
+        <v-chip
+          v-for="number in item.resultado"
+          :key="number.numero"
+          color="primary"
+        >
+          {{ getFormattedNumber(number.numero) }}
+        </v-chip>
+      </template>
+      <template
+        v-slot:[`item.occurrences`]="{ item }"
+      >
+        {{ item.apariciones }}
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+import getFormattedNumber from '@/mixins/getFormattedNumber';
+
+export default {
+  name: 'PrimitivaOccurrencesByResultStatsTable',
+  mixins: [
+    getFormattedNumber,
+  ],
+  data() {
+    return {
+      options: {
+        mustSort: true,
+        sortBy: ['occurrences'],
+        sortDesc: [true],
+        itemsPerPage: this.$store.state.primitiva.statsPagination.per_page,
+      },
+      headers: [
+        {
+          text: this.$t('VIEWS.PRIMITIVA.STATS.OCCURRENCES_BY_RESULT.TABLE.RESULT.LABEL'),
+          align: 'center',
+          sortable: true,
+          value: 'result',
+        },
+        {
+          text: this.$t('VIEWS.PRIMITIVA.STATS.OCCURRENCES_BY_RESULT.TABLE.OCCURRENCES.LABEL'),
+          align: 'center',
+          sortable: true,
+          value: 'occurrences',
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState('primitiva', {
+      loading: 'loading',
+      pagination: 'statsPagination',
+      occurrencesByResultStats: (state) => state.stats.occurrencesByResult,
+    }),
+  },
+  watch: {
+    options: {
+      handler() {
+        this.getOccurrencesByResultStats();
+      },
+      deep: true,
+    },
+  },
+  destroyed() {
+    this.$store.dispatch('primitiva/destroyStats');
+  },
+  methods: {
+    getOccurrencesByResultStats() {
+      const {
+        sortBy, sortDesc, page, itemsPerPage,
+      } = this.options;
+
+      this.$store.commit('primitiva/setStatsPagination', {
+        page,
+        per_page: itemsPerPage,
+        sort_type: sortDesc[0] ? 'desc' : 'asc',
+        sort_property: sortBy[0],
+      });
+
+      this.$store.dispatch('primitiva/getOccurrencesByResultStats');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
