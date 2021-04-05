82ad13a16da1c3356777473f5f769af847430157
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2021-04-05 23:15:26 +0200

    #24 (euromillones/stats/*): Implementación de sección de estadísticas de Euromillones

---
+++ components/euromillones/stats/EuromillonesOccurrencesByResultStatsTable.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="occurrencesByResultStats.data"
+      :options.sync="options"
+      :server-items-length="occurrencesByResultStats.total"
+      :loading="loading"
+      class="elevation-1"
+    >
+      <template v-slot:progress>
+        <v-progress-linear
+          indeterminate
+          absolute
+          color="red darken-2"
+        />
+      </template>
+      <template
+        v-slot:[`item.result`]="{ item }"
+      >
+        <v-chip
+          v-for="number in item.resultado"
+          :key="number.numero"
+          color="primary"
+        >
+          {{ getFormattedNumber(number.numero) }}
+        </v-chip>
+      </template>
+      <template
+        v-slot:[`item.occurrences`]="{ item }"
+      >
+        {{ item.apariciones }}
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+import getFormattedNumber from '@/mixins/getFormattedNumber';
+
+export default {
+  name: 'EuromillonesOccurrencesByResultStatsTable',
+  mixins: [
+    getFormattedNumber,
+  ],
+  data() {
+    return {
+      options: {
+        mustSort: true,
+        sortBy: ['occurrences'],
+        sortDesc: [true],
+        itemsPerPage: this.$store.state.euromillones.statsPagination.per_page,
+      },
+      headers: [
+        {
+          text: this.$t('VIEWS.EUROMILLONES.STATS.OCCURRENCES_BY_RESULT.TABLE.RESULT.LABEL'),
+          align: 'center',
+          sortable: true,
+          value: 'result',
+        },
+        {
+          text: this.$t('VIEWS.EUROMILLONES.STATS.OCCURRENCES_BY_RESULT.TABLE.OCCURRENCES.LABEL'),
+          align: 'center',
+          sortable: true,
+          value: 'occurrences',
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState('euromillones', {
+      loading: 'loading',
+      pagination: 'statsPagination',
+      occurrencesByResultStats: (state) => state.stats.occurrencesByResult,
+    }),
+  },
+  watch: {
+    options: {
+      handler() {
+        this.getOccurrencesByResultStats();
+      },
+      deep: true,
+    },
+  },
+  destroyed() {
+    this.$store.dispatch('euromillones/destroyStats');
+  },
+  methods: {
+    getOccurrencesByResultStats() {
+      const {
+        sortBy, sortDesc, page, itemsPerPage,
+      } = this.options;
+
+      this.$store.commit('euromillones/setStatsPagination', {
+        page,
+        per_page: itemsPerPage,
+        sort_type: sortDesc[0] ? 'desc' : 'asc',
+        sort_property: sortBy[0],
+      });
+
+      this.$store.dispatch('euromillones/getOccurrencesByResultStats');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
