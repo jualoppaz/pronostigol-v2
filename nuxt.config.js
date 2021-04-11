705d381685285872153a5baf9473191d3bf048df
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2021-04-11 18:22:47 +0200

    #28 (nuxt.config.js): Modificación de la plantilla para los títulos de las páginas de la aplicación

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - Pronostigol',
+    titleTemplate: '%s | PRONOSTIGOL',
     title: 'pronostigol-v2',
     meta: [
       { charset: 'utf-8' },
