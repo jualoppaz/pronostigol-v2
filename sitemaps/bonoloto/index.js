35c67089e91c3e90effc244c89e0f1a07ca8a852
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2021-04-14 22:10:47 +0200

    #26 (sitemaps): Implementación de generación dinámica de sitemaps con todas las páginas de la web clasificadas por sección

---
+++ sitemaps/bonoloto/index.js
@@ -0,0 +1,27 @@
+const game = 'bonoloto';
+
+export default {
+  path: `/sitemap-${game}.xml`,
+  gzip: true,
+  lastmod: '2021-04-14',
+  exclude: ['/**'],
+  routes() {
+    return [
+      {
+        url: `/${game}`,
+        changefreq: 'yearly',
+        priority: 0.8,
+      },
+      {
+        url: `/${game}/sorteos`,
+        changefreq: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: `/${game}/estadisticas`,
+        changefreq: 'weekly',
+        priority: 0.8,
+      },
+    ];
+  },
+};
