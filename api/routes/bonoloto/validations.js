730bf56a4bc86b7ee4eb0fbb7ca5e4595a2faa78
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2021-05-10 21:42:48 +0200

    #31 (api/routes/bonoloto/validations): Implementación de validaciones de los distintos endpoints de estadísticas

---
+++ api/routes/bonoloto/validations.js
@@ -0,0 +1,85 @@
+const { Joi } = require('express-validation');
+
+module.exports = {
+  getTickets: {
+    query: Joi.object({
+      year: Joi.number(),
+      raffle: Joi.number().min(1),
+      page: Joi.number().min(1),
+      per_page: Joi.number().min(1),
+      sort_type: Joi.string().valid('asc', 'desc'),
+    }),
+  },
+  getOccurrencesByNumber: {
+    query: Joi.object({
+      page: Joi.number()
+        .min(1)
+        .required(),
+      per_page: Joi.number()
+        .min(1)
+        .required(),
+      sort_type: Joi.string().valid('asc', 'desc'),
+      sort_property: Joi.string().valid('number', 'occurrences'),
+    }),
+  },
+  getOccurrencesByReimbursement: {
+    query: Joi.object({
+      page: Joi.number()
+        .min(1)
+        .required(),
+      per_page: Joi.number()
+        .min(1)
+        .required(),
+      sort_type: Joi.string().valid('asc', 'desc'),
+      sort_property: Joi.string().valid('reimbursement', 'occurrences'),
+    }),
+  },
+  getOccurrencesByResultWithReimbursement: {
+    query: Joi.object({
+      page: Joi.number()
+        .min(1)
+        .required(),
+      per_page: Joi.number()
+        .min(1)
+        .required(),
+      sort_type: Joi.string().valid('asc', 'desc'),
+      sort_property: Joi.string().valid('result', 'occurrences'),
+    }),
+  },
+  getOccurrencesByResult: {
+    query: Joi.object({
+      page: Joi.number()
+        .min(1)
+        .required(),
+      per_page: Joi.number()
+        .min(1)
+        .required(),
+      sort_type: Joi.string().valid('asc', 'desc'),
+      sort_property: Joi.string().valid('result', 'occurrences'),
+    }),
+  },
+  getLastDateByNumber: {
+    query: Joi.object({
+      page: Joi.number()
+        .min(1)
+        .required(),
+      per_page: Joi.number()
+        .min(1)
+        .required(),
+      sort_type: Joi.string().valid('asc', 'desc'),
+      sort_property: Joi.string().valid('number', 'date'),
+    }),
+  },
+  getLastDateByReimbursement: {
+    query: Joi.object({
+      page: Joi.number()
+        .min(1)
+        .required(),
+      per_page: Joi.number()
+        .min(1)
+        .required(),
+      sort_type: Joi.string().valid('asc', 'desc'),
+      sort_property: Joi.string().valid('reimbursement', 'date'),
+    }),
+  },
+};
