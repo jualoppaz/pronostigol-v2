46cb2a5171d538a8cfd97b5c7e61c2add0a7e539
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2021-09-04 14:34:21 +0200

    #31 (api/models): Especificación de colección en los modelos de La Quiniela

--- api/models/quinielaCompetition.js
+++ api/models/quinielaCompetition.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+const collectionName = 'quiniela_competitions';
+
 const quinielaCompetitionSchema = new Schema({
   name: { type: String, required: true },
   value: { type: String, required: true },
+}, {
+  collection: collectionName,
 });
 
 module.exports = mongoose.model('QuinielaCompetition', quinielaCompetitionSchema);
