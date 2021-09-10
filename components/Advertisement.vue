fa890e7c9a7486c54c3caac5e3ab603b02bed97c
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2021-09-10 18:06:15 +0200

    #35 (components/Advertisement.vue): add component for display google ads

---
+++ components/Advertisement.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="advertisement">
+    <adsbygoogle
+      data-ad-slot="6492981921"
+      data-ad-format="fluid"
+      data-full-width-responsive="true"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Advertisement',
+};
+</script>
+
+<style lang="scss" scoped>
+.advertisement{
+  margin-top: 15px;
+}
+</style>
