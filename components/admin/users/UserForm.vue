1fcad972b2b143d70d180d657afaf8c61074c199
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2022-02-04 19:10:40 +0100

    #45 enhancement (components/admin/users/UserForm.vue): add user form

--- components/admin/users/UserForm.vue
+++ components/admin/users/UserForm.vue
@@ -10,20 +10,20 @@
           md="6"
         >
           <v-text-field
-            id="user"
-            v-model="user"
+            v-model="form.user"
             :rules="rules.user"
             :label="userText"
             clearable
           />
         </v-col>
+      </v-row>
+      <v-row>
         <v-col
           cols="12"
           md="6"
         >
           <v-autocomplete
-            id="role"
-            v-model="role"
+            v-model="form.role"
             :rules="rules.role"
             :items="roles"
             :label="roleText"
@@ -33,17 +33,39 @@
           />
         </v-col>
       </v-row>
-      <v-row
-        justify="center"
-      >
+      <v-row>
         <v-col
-          cols="auto"
+          cols="12"
+          md="6"
+        >
+          <v-text-field
+            v-model="form.password"
+            type="password"
+            :rules="rules.password"
+            :label="passwordText"
+            clearable
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="12"
+          md="6"
         >
           <v-btn
+            nuxt
+            :to="localePath({
+              name: 'admin-users'
+            })"
+          >
+            {{ cancelButtonText }}
+          </v-btn>
+          <v-btn
+            class="float-right"
             color="primary"
             @click="submitForm"
           >
-            {{ searchText }}
+            {{ submitButtonText }}
           </v-btn>
         </v-col>
       </v-row>
@@ -60,13 +82,24 @@ export default {
   data() {
     return {
       rules: {
-        user: [],
-        role: [],
+        user: [
+          (v) => !!v || 'xoxName is required',
+        ],
+        role: [
+          (v) => !!v || 'xoxRole is required',
+        ],
+        password: [
+          (v) => !!v || 'xoxPassword is required',
+        ],
       },
       valid: false,
+      form: {
+        user: '',
+        role: null,
+        password: '',
+      },
       userText: this.$t('DASHBOARD.VIEWS.USERS.USER_FORM.USER.LABEL'),
       roleText: this.$t('DASHBOARD.VIEWS.USERS.USER_FORM.ROLE.LABEL'),
-      searchText: this.$t('VIEWS.QUINIELA.STATS.CUSTOM_STATS_FORM.FILTERS.SEARCH.TEXT'),
       roles: [
         {
           name: this.$t('ROLES.ADMIN.TEXT'),
@@ -76,14 +109,21 @@ export default {
           value: utils.ROLES.PRIVILEGED.VALUE,
         },
       ],
+      passwordText: this.$t('DASHBOARD.VIEWS.USERS.USER_FORM.PASSWORD.LABEL'),
+      cancelButtonText: this.$t('COMMON.BUTTON.CANCEL.TEXT'),
+      submitButtonText: this.$t('COMMON.BUTTON.SUBMIT.TEXT'),
     };
   },
   computed: {},
   methods: {
     async validateForm() {
-      this.$refs.form.validate();
+      this.$refs.userForm.validate();
 
       return this.valid;
+    },
+    async submitForm() {
+      const result = await this.validateForm();
+      if (result) this.$emit('onSubmit');
     },
   },
 };
