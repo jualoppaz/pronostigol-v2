9d4fb8e2fbd47a3ac28391fe2c27b57f669d0fc4
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2022-02-04 19:05:43 +0100

    #45 enhancement (api/routes/users/validations.js): add validation for POST /users resource

--- api/routes/users/validations.js
+++ api/routes/users/validations.js
@@ -2,12 +2,19 @@ const { Joi } = require('express-validation');
 
 module.exports = {
   getUsers: {
-    body: Joi.object({
+    query: Joi.object({
       user: Joi.string(),
       page: Joi.number().min(1),
       per_page: Joi.number().min(1),
       sort_type: Joi.string().valid('asc', 'desc'),
       sort_property: Joi.string().valid('user'),
+    }),
+  },
+  createUser: {
+    body: Joi.object({
+      user: Joi.string().required(),
+      role: Joi.string().allow('admin', 'privileged').required(),
+      password: Joi.string().required(),
     }),
   },
 };
