682c245ba784a920824083221fe3832a81085048
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2022-08-19 18:04:03 +0200

    #49 (api/routes/quiniela/validations): add validation for getTeams method

--- api/routes/quiniela/validations.js
+++ api/routes/quiniela/validations.js
@@ -15,7 +15,13 @@ module.exports = {
   },
   getTeams: {
     query: Joi.object({
+      page: Joi.number()
+        .min(1),
+      per_page: Joi.number()
+        .min(1),
       sort_type: Joi.string().valid('asc', 'desc'),
+      sort_property: Joi.string().valid('name'),
+      name: Joi.string(),
     }),
   },
   getSeasons: {
